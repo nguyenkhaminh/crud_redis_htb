074e7f8f386f17115725abaaf420cb9997a02c0a
nguyenkhaminh <69241622+nguyenkhaminh@users.noreply.github.com>
2021-11-08 10:30:04 +0700

    Add files via upload

---
+++ crud-redis-master/src/main/java/iuh/minh/crudredis/CrudRedisApplication.java
@@ -0,0 +1,13 @@
+package iuh.nctd.crudredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRedisApplication.class, args);
+	}
+
+}
---
+++ crud-redis-master/src/test/java/iuh/minh/crudredis/CrudRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.nctd.crudredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
